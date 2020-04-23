c1cba487ec006e25a6906b7766a302cdb21a7adb
20100000 <tiago_honorio2010@hotmail.com>
2020-04-23 11:16:18 -0300

    add es6 e status code de erro

--- api/pokemon/controller.js
+++ api/pokemon/controller.js
@@ -1,54 +1,70 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
+
+const getTipePk = (temp) =>{
+    let type = null;
+    if (temp< 5) {
+        type = 'ice';
+    } else if (temp>= 5 && temp< 10) {
+        type = 'water';
+    } else if (temp>= 12 && temp< 15) {
+        type = 'glass';
+    } else if (temp>= 15 && temp< 21) {
+        type = 'ground';
+    } else if (temp>= 23 && temp< 27) {
+        type = 'bug';
+    } else if (temp>= 27 && temp<= 33) {
+        type = 'rock';
+    } else if (temp> 33) {
+        type = 'fire';
+    } else {
+        type = 'normal'
+    }
+    return type;
+}
+
+const getPokemon = async (type) => {
+    const url = `https://pokeapi.co/api/v2/type/${type}`;
+    const responsePokemon = await fetch(url);
+    const resPokemon = await responsePokemon.json();
+    const pokemon = resPokemon.pokemon[Math.floor(Math.random() * resPokemon.pokemon.length)].pokemon;
+    return pokemon;
+}
+
+const getImagePokemon = async (pokemon) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
+    const responsePokemonImg = await fetch(url);
+    const resPokemonImg = await responsePokemonImg.json();
+    const pokemonImg = resPokemonImg.sprites.front_default
+    return pokemonImg;
+}
 
 const _getPokemon = async (req, res, next) => {
     let temp;
     const city = req.params.city;
     const keyApi = process.env.PASSWORD_WEATHER;
-    let typePokemon = '';
+    let typePokemon = null;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${keyApi}&units=metric`;
 
     try {
         const response = await fetch(url);
         const body = await response.json();
-        let rain = body.weather[0].main;
+        const rain = body.weather[0].main;
         temp = body.main.temp;
 
-        if (body.main.temp < 5) {
-            typePokemon = 'ice';
-        } else if (body.main.temp >= 5 && body.main.temp < 10) {
-            typePokemon = 'water';
-        } else if (body.main.temp >= 12 && body.main.temp < 15) {
-            typePokemon = 'glass';
-        } else if (body.main.temp >= 15 && body.main.temp < 21) {
-            typePokemon = 'ground';
-        } else if (body.main.temp >= 23 && body.main.temp < 27) {
-            typePokemon = 'bug';
-        } else if (body.main.temp >= 27 && body.main.temp <= 33) {
-            typePokemon = 'rock';
-        } else if (body.main.temp > 33) {
-            typePokemon = 'fire';
-        } else {
-            typePokemon = 'normal'
-        }
-        //
         if (rain === 'Rain' || rain === 'Thunderstorm') {
             typePokemon = 'electric';
+        } else {
+            typePokemon =  getTipePk(temp);
         }
 
-        url = `https://pokeapi.co/api/v2/type/${typePokemon}`;
-        const responsePokemon = await fetch(url);
-        const resPokemon = await responsePokemon.json();
-        const pokemon = resPokemon.pokemon[Math.floor(Math.random() * resPokemon.pokemon.length)].pokemon;
+        const pokemon = await getPokemon(typePokemon);
 
-        url = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
-        const responsePokemonImg = await fetch(url);
-        const resPokemonImg = await responsePokemonImg.json();
-        const pokemonImg = resPokemonImg.sprites.front_default
+        const pokemonImg = await getImagePokemon(pokemon);
 
         return res.send(JSON.stringify({success: true, pokemon: pokemon, rain: rain, temp: temp, img: pokemonImg}));
     } catch (err) {
+         res.status(500);
          return res.send(JSON.stringify({success: false, error: err}));
-        next(err);
     }
 };
 
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-var cors = require('cors');
-var cons = require('consolidate');
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+let cors = require('cors');
+let cons = require('consolidate');
 
 const pokemon = require('./api/pokemon/router');
 //Reservar
@@ -25,12 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(pokemon.path, pokemon.router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
